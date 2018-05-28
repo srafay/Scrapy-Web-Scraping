41ac39b6f18585e30b6dfb22ed7ff0566f71fae9
srafay <rafay_syed1@yahoo.com>
2018-05-28 11:56:40 +0500

    Generalized the scrapper, can give input category by running the script with -a category=input . It automatically separates items into sub-categories also

--- AlternativeTo/AlternativeTo/spiders/getSubCategories.py
+++ AlternativeTo/AlternativeTo/spiders/getSubCategories.py
@@ -1,31 +1,31 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
-
 class GetsubcategoriesSpider(scrapy.Spider):
     name = 'getSubCategories'
 
     def __init__(self, category=None, *args, **kwargs):
+	global categoryToBeScrapped
+	categoryToBeScrapped = category
 	super(GetsubcategoriesSpider, self).__init__(*args, **kwargs)
 	self.start_urls = ['http://alternativeto.net/category/' + category]
-#    start_urls = ['http://alternativeto.net/category/games//']
 
     # Entry point for the spider
     def parse(self, response):
-	categoryIndex = 0
+	subCategoryIndex = 0
 	for href in response.css("div[class='content-box']>h2>a[href^='//alternativeto.net/category']::attr(href)"):
 		url = href.extract()
 		url = "https://" + url[2:]
-		category = response.css("div[class='content-box']>h2>a[href^='//alternativeto.net/category']::text").extract()[categoryIndex]
-		categoryIndex = categoryIndex + 1
-		yield scrapy.Request(url, callback=self.parse_category, meta={'category': category})
+		subCategory = response.css("div[class='content-box']>h2>a[href^='//alternativeto.net/category']::text").extract()[subCategoryIndex]
+		subCategoryIndex = subCategoryIndex + 1
+		yield scrapy.Request(url, callback=self.parse_category, meta={'subCategory': subCategory})
 
     # Method for parsing a category
     def parse_category(self, response):
-	category = response.meta.get('category')
+	subCategory = response.meta.get('subCategory')
 	for href in response.css("a[data-link-action='DefaultSort']::attr(href)"):
 		url = href.extract()
-		yield scrapy.Request("https://alternativeto.net" + url, callback=self.parse_item, meta={'category': category})
+		yield scrapy.Request("https://alternativeto.net" + url, callback=self.parse_item, meta={'subCategory': subCategory})
 
     # Method for parsing an item
     def parse_item(self, response):
@@ -34,7 +34,7 @@ class GetsubcategoriesSpider(scrapy.Spider):
 	votes = response.css('.like-box-wrapper > div:nth-child(2) > span:nth-child(1)::text').extract()
 	lic = response.css('.labels > li:nth-child(1) > span:nth-child(1) > span:nth-child(1)::text').extract()
 	platforms = response.css("li.label::text").extract()
-	category = response.meta.get('category')
+	subCategory = response.meta.get('subCategory')
 
         #Give the extracted content row wise
         for item in zip(lic, description, votes, name):
@@ -44,8 +44,9 @@ class GetsubcategoriesSpider(scrapy.Spider):
                 'description' : item[1],
                 'votes' : item[2],
                 'name' : item[3],
-                'category' : category,
+                'Sub-Category' : subCategory,
                 'platforms' : str(platforms),
+		'Category' : categoryToBeScrapped,
             }
             #yield or give the scraped info to scrapy
             yield scraped_info
