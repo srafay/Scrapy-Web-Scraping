1b913b477fa9ef75a9ecf55e6641d5f160cfb6f4
srafay <rafay_syed1@yahoo.com>
2018-05-28 10:29:05 +0500

    Modified the program to scrap every category from 'games'. Now need to just include category 'type' in the storage file

--- AlternativeTo/AlternativeTo/spiders/getSubCategories.py
+++ AlternativeTo/AlternativeTo/spiders/getSubCategories.py
@@ -4,8 +4,40 @@ import scrapy
 
 class GetsubcategoriesSpider(scrapy.Spider):
     name = 'getSubCategories'
-    allowed_domains = ['www.alternativeto.net/category/games/']
     start_urls = ['http://alternativeto.net/category/games//']
 
+    # Entry point for the spider
     def parse(self, response):
-        pass
+	for href in response.css("div[class='content-box']>h2>a[href^='//alternativeto.net/category']::attr(href)"):
+		url = href.extract()
+		url = "https://" + url[2:]
+		category = response.css("div[class='content-box']>h2>a[href^='//alternativeto.net/category']::text")
+		yield scrapy.Request(url, callback=self.parse_category)
+
+    # Method for parsing a category
+    def parse_category(self, response):
+	for href in response.css("a[data-link-action='DefaultSort']::attr(href)"):
+		url = href.extract()
+		yield scrapy.Request("https://alternativeto.net" + url, callback=self.parse_item)
+
+    # Method for parsing an item
+    def parse_item(self, response):
+	name = response.css('#appHeader > div:nth-child(1) > div:nth-child(1) > h1:nth-child(2)::text').extract()
+	description = response.css('.item-desc > p:nth-child(1)::text').extract()
+	votes = response.css('.like-box-wrapper > div:nth-child(2) > span:nth-child(1)::text').extract()
+	lic = response.css('.labels > li:nth-child(1) > span:nth-child(1) > span:nth-child(1)::text').extract()
+	platforms = response.css("li.label::text").extract()
+
+        #Give the extracted content row wise
+        for item in zip(name, description, votes, lic):
+            #create a dictionary to store the scraped info
+            scraped_info = {
+                'name' : item[0],
+                'description' : item[1],
+                'votes' : item[2],
+                'license' : item[3],
+                'platforms' : str(platforms),
+            }
+            #yield or give the scraped info to scrapy
+            yield scraped_info
+
